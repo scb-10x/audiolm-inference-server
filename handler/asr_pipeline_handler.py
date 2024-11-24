4a4155f778f6d81228e2997adf1e15cf545ad6a7
scb10x <typhoon@scb10x.com>
2024-11-24 09:16:31 -0800

    add asr-pipeline version

---
+++ handler/asr_pipeline_handler.py
@@ -0,0 +1,125 @@
+import os
+import time
+from typing import AsyncGenerator
+from fastapi.responses import StreamingResponse
+from utils import get_either, get_file_from_any
+from vllm import AsyncEngineArgs, AsyncLLMEngine, SamplingParams
+from entity.entity import (
+    ChatCompletionResponseStreamChoice,
+    ChatCompletionStreamResponse,
+    DeltaMessage,
+    UsageInfo,
+)
+from handler.base_handler import BaseHandler
+from transformers import AutoProcessor
+from transformers import pipeline
+
+
+class ASRPipelineHandler(BaseHandler):
+
+    def __init__(self, model_name: str):
+        super().__init__(model_name)
+        engine_args = AsyncEngineArgs(
+            model=model_name,
+            max_model_len=2048,
+            gpu_memory_utilization=float(os.environ.get("GPU_MEMORY_UTILIZATION", 0.4)),
+            enforce_eager=True,
+        )
+        llm = AsyncLLMEngine.from_engine_args(engine_args)
+        self.processor = AutoProcessor.from_pretrained(model_name)
+        self.llm = llm
+        asr_pipe = pipeline(
+            task="automatic-speech-recognition",
+            model="openai/whisper-large-v3-turbo",
+            chunk_length_s=30,
+            device="cuda",
+        )
+        self.asr_pipe = asr_pipe
+
+    def generate_stream(self, request):
+        for message in request.messages:
+            if isinstance(message["content"], list):
+                cur_audio_text = None
+                for ele in message["content"]:
+                    if ele["type"] == "audio":
+                        cur_audio_text = self.asr_pipe(
+                            get_file_from_any(a),
+                            generate_kwargs={"task": "transcribe"},
+                            return_timestamps=False,
+                        )["text"]
+                if cur_audio_text is not None:
+                    for ele in message["content"]:
+                        if ele["type"] == "text":
+                            if '<audio>' in ele['content']:
+                                ele["content"].replace("<audio>", cur_audio_text)
+                            else:
+                                ele['content'] = ele['content'] + cur_audio_text
+        
+        engine_prompt = self.processor.apply_chat_template(
+            request.messages,
+            add_generation_prompt=True,
+            tokenize=False,
+        )
+        results_generator = self.llm.generate(
+            engine_prompt,
+            SamplingParams(
+                top_p=request.top_p,
+                temperature=request.temperature,
+                top_k=request.top_k,
+                repetition_penalty=request.repetition_penalty,
+                max_tokens=get_either(
+                    [request.max_completion_tokens, request.max_tokens]
+                ),
+            ),
+            str(time.monotonic()),
+        )
+
+        # Streaming case
+        async def stream_results() -> AsyncGenerator[bytes, None]:
+            prev_output = ""
+            async for request_output in results_generator:
+                output = request_output.outputs[-1]
+                delta_out = output.text[len(prev_output) :]
+                choice_data = ChatCompletionResponseStreamChoice(
+                    index=0,
+                    delta=DeltaMessage(content=delta_out),
+                    logprobs=None,
+                    finish_reason=output.finish_reason,
+                )
+
+                chunk = ChatCompletionStreamResponse(
+                    id=request.request_id,
+                    object="chat.completion.chunk",
+                    created=int(time.time()),
+                    choices=[choice_data],
+                    model=self.model_name,
+                )
+                chunk.usage = UsageInfo(
+                    prompt_tokens=0, completion_tokens=0, total_tokens=0
+                )
+                data = chunk.model_dump_json(exclude_unset=True)
+                yield f"data: {data}\n\n"
+                prev_output = output.text
+            choice_data = ChatCompletionResponseStreamChoice(
+                index=0,
+                delta=DeltaMessage(content=""),
+                logprobs=None,
+                finish_reason=output.finish_reason,
+            )
+            final_usage_chunk = ChatCompletionStreamResponse(
+                id=request.request_id,
+                object="chat.completion.chunk",
+                created=int(time.time()),
+                choices=[choice_data],
+                model=self.model_name,
+                usage=UsageInfo(prompt_tokens=0, completion_tokens=0, total_tokens=0),
+            )
+            final_usage_data = final_usage_chunk.model_dump_json(
+                exclude_unset=True, exclude_none=True
+            )
+            yield f"data: {final_usage_data}\n\n"
+
+        return StreamingResponse(stream_results())
+
+    def generate(self, request):
+        raise NotImplementedError
--- handler/handler.py
+++ handler/handler.py
@@ -1,7 +1,11 @@
 from .huggingface_handler import HuggingfaceHandler
 from .vllm_handler import VLLMHandler
+from .asr_pipeline_handler import ASRPipelineHandler
 
 def get_handler(model_name: str):
     if model_name in ['Qwen/Qwen2-Audio-7B-Instruct']:
         return VLLMHandler(model_name)
+    elif 'pipeline/' in model_name:
+        llm_model_name = model_name.replace('pipeline/', '')
+        return ASRPipelineHandler(llm_model_name)
     return HuggingfaceHandler(model_name)
